f3db6b04a292a15e348f94627fbf3590b0c05a50
Isha Gupta <ishagpt8@gmail.com>
2019-10-15 09:21:23 +0530

    first commit

--- Team/teammembers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TeammembersConfig(AppConfig):
-    name = 'teammembers'
--- Team/teammembers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import TeamMemberListView
-
-app_name = "teammembers" # helps do a reverse lookup later
-
-urlpatterns = [
-    path('teammembers/', TeamMemberListView.as_view()),
-    path('teammembers/<int:pk>', TeamMemberListView.as_view()),
-]
